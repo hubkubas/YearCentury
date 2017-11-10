1ea80438c9298c370428e51648c5a2e9f662d0c8
Hubert Kubasiewicz <hkubasiewicz@italents.pl>
2017-11-10 22:11:22 +0000

    Initial commit

---
+++ century.js
@@ -0,0 +1,13 @@
+var year = 1956;
+
+function centuryFromYear(year) {
+if(year % 100 == 0) {
+var wiek = Math.floor(year / 100);
+return wiek;
+} else {
+var wiek = Math.floor((year / 100)+1);
+return wiek;
+}
+}
+
+console.log(centuryFromYear(year)+" century");
